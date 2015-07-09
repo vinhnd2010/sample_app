b4062e350c9c94ed080f3612e695027917cb2ab7
Nguyen Duc Vinh <nguyenducvinh92@gmail.com>
2015-07-09 15:15:21 +0700

    check heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,10 +15,19 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   host = "localhost:3000"
   config.action_mailer.default_url_options = {host: host}
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'nguyenducvinh92@gmail.com',
+    :password       => '246810121',
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
