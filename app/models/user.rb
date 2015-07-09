403737211fc1c5674b03dcd3572634b85be7dc18
Nguyen Duc Vinh <nguyenducvinh92@gmail.com>
2015-07-09 14:00:07 +0700

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
 
-  	validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
@@ -51,17 +51,33 @@ class User < ActiveRecord::Base
     UserMailer.account_activation(self).deliver_now
   end
 
-	private
+  def create_reset_digest
+  	self.reset_token = User.new_token
+  	update_attribute(:reset_digest, User.digest(reset_token))
+  	update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
-		# Converts emal to all lower-case
-		def downcase_email
-			self.email = email.downcase
-		end
+  # Sends password reset email
+  def send_password_reset_email
+  	UserMailer.password_reset(self).deliver_now
+  end
 
-		# Creates and assigns the activation token and digest
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
-		def create_activation_digest
-			self.activation_token = User.new_token
-			self.activation_digest = User.digest(activation_token)
-		end
+private
+
+	# Converts emal to all lower-case
+	def downcase_email
+		self.email = email.downcase
+	end
+
+	# Creates and assigns the activation token and digest
+
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest = User.digest(activation_token)
+	end
 end
